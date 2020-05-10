d816ba077a78949cff2ace4f5380a7ba6fdfa4a0
Jeffrey Otoibhi <jeffreyotoibhi@gmail.com>
2020-05-10 22:29:51 +0100

    pickled

--- app.py
+++ app.py
@@ -4,17 +4,44 @@ import gzip
 from os.path import join,abspath,curdir
 from pathlib import Path
 import dill
+import pickle
+from html import unescape
+import spacy
+from spacy.lang.en import STOP_WORDS
+nlp = spacy.load('en_core_web_sm',disable = ['ner','parser','tagger'])
 
 app = Flask(__name__)
 
+
+days = ['monday','tuesday','wednesday', 'thursday', 'friday', 'saturday','sunday','sun','mon','tue','wed','thur','fri','sat']
+
+def remove_day(doc):
+    doc = doc.lower()
+    for word in doc.split(' '):
+        if word in days:
+            doc = doc.replace(word,'')
+    return doc
+
+
+def preprocessor(doc):
+    doc = remove_day(doc)
+    return unescape(doc)
+
+def tokenizer(doc):
+    return [word.lemma_ for word in nlp(doc)]
+
+
+
+
 cwd = abspath(curdir)
-model_path = join(cwd, 'sentiment_ng_model.dill.gz')
+model_path = join(cwd, 'sentiment_ng_model.pkl')
 path = Path(model_path).as_posix()
 
 
 @app.route('/about')
 def about():
-    return render_template('developer.html')
+    #return render_template('developer.html')
+    return path
 
 @app.route('/projects')
 def projects():
@@ -37,8 +64,8 @@ def predict():
         tweet = request.args.get('tweet')
     else:
         tweet = request.form['text']
-    with gzip.open(path, 'rb') as f:
-        model = dill.load(f)
+    with open(path, 'rb') as f:
+        model = pickle.load(f)
     proba = round(model.predict_proba([tweet])[0,1]* 100,2)
     if (proba <= 52 and proba >= 48):
         sent = -1
