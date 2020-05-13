dc7549218361354e865f7f6f8bde148a0d6902fb
Jeffrey Otoibhi <jeffreyotoibhi@gmail.com>
2020-05-13 21:45:48 +0100

    commented code

--- Sent_Analysis.py
+++ Sent_Analysis.py
@@ -14,6 +14,12 @@ from xgboost import XGBClassifier
 from sklearn.pipeline import Pipeline
 import spacy
 from spacy.lang.en import STOP_WORDS
+
+'''This file contains classes and functions used to train the naive bayes and SDC classifier
+    on about 1.5 million tweets from twitter.Dataset available at http://thinknook.com/twitter-sentiment-analysis-training-corpus-dataset-2012-09-22'''
+
+#Reduce each word in STOP_WORDS to its lemma.
+#Remove 'not', 'so','very','cannot','can' from the stop words because they are relevant for sentiment analysis.
 nlp = spacy.load('en_core_web_sm',disable = ['ner','parser','tagger'])
 words = ['not','so','very','cannot','can']
 for word in words:
@@ -24,9 +30,11 @@ STOP_WORDS_lemma = set(STOP_WORDS_lemma).union({' ,','.',';'})
 
 
 
-
+#create a list of weekdays 
 days = ['monday','tuesday','wednesday', 'thursday', 'friday', 'saturday','sunday','sun','mon','tue','wed','thur','fri','sat']
+
 class OnlinePipeline(Pipeline):
+    '''This class creates an online pipeline to enable a model learn online'''
     def partial_fit(self,X,y):
         try:
             Xt = X.copy()
@@ -43,6 +51,7 @@ class OnlinePipeline(Pipeline):
         return self
 
 def fit_model(func):
+    ''' wrapper function that trains and returns model using given dataset.'''
     def wrapper(*args, **kwargs):
         t_0 = time.time()
         model = func()
@@ -57,23 +66,7 @@ def fit_model(func):
 
     return wrapper
 
-def partial_fit(func):
-    def wrapper(*args, **kwargs):
-        t_0 = time.time()
-        model = func()
-        model.fit(X_train, y_train)
-        t_elapsed = time.time() - t_0
-
-        print(f'Training time : {t_elapsed}')
-        print(f'Training accuracy : {model.score(X_train,y_train)}')
-        print(f'Testing accuracy : {model.score(X_test,y_test)}')
-
-        return model
-
-    return wrapper
-
-
-
+## function removes weekdays from text and transforms text to lowercase.
 def remove_day(doc):
     doc = doc.lower()
     for word in doc.split(' '):
@@ -81,15 +74,16 @@ def remove_day(doc):
             doc = doc.replace(word,'')
     return doc
 
-
+#calls the remove_day function and unescapes html characters from tweets/text.
 def preprocessor(doc):
     doc = remove_day(doc)
     return unescape(doc)
 
+#tokenizer function to be passed to the transformers.
 def tokenizer(doc):
     return [word.lemma_ for word in nlp(doc)]
 
-
+#function creates an online model using the sgdclassifier and the hashing vectorizer
 @fit_model
 def online_model():
     from sklearn.linear_model import SGDClassifier
@@ -103,7 +97,7 @@ def online_model():
 
     return pipe
 
-
+#function creates an online model using the tfidf vectorizer and sgdclassifier
 @fit_model
 def online_tfidf_model():
     from sklearn.linear_model import SGDClassifier
@@ -117,7 +111,7 @@ def online_tfidf_model():
 
     return pipe
 
-
+#function creates an offline model using tfidfvectorizer and naive bayes classifier.
 @fit_model
 def construct_model():
     vectorizer = TfidfVectorizer(preprocessor=preprocessor,
@@ -129,7 +123,7 @@ def construct_model():
     pipe = Pipeline([('vectorizer', vectorizer), ('classifier', clf)])
     return pipe
 
-
+#function creates a model, trains it and serialized it in a gzip file. it takes a function as argument
 def serialize_model(func):
     #model = construct_model()
     model=  func()
@@ -137,13 +131,14 @@ def serialize_model(func):
     with gzip.open('sentiment_ng_model.dill.gz', 'wb') as f:
         dill.dump(model, f, recurse = True)
 
-
+#function does the same as above but using the pickle library
 def pickle_model(func):
     model = func()
     with open('sentiment_ng_model.pkl','wb') as f:
         pickle.dump(model,f)
     print('Done')
 
+#This function further processes the dataset in the csv file to remove the most frequent words and tags.
 def remove_rare(df):
     print('Removing the rare words....')
     freq = pd.Series(' '.join(df['SentimentText']).split()).value_counts()[-200:]
@@ -153,23 +148,27 @@ def remove_rare(df):
     return df
 
 if __name__ == '__main__':
-    print('initiating training......')
+    print('Initiating training......')
+
+    #Read csv file
     df = pd.read_csv('Sentiment Analysis Dataset.csv', error_bad_lines= False)
-
+    
+    #run the remove_rare function
     df = remove_rare(df)
-
     print('Removing punctuations.....')
+     
+    #create both data and target/labels. Remove all punctuations from data.
     X= df['SentimentText'].str.replace('[^\w\s]','')
     y = df['Sentiment']
 
+    #Create train and test data by splitting original data.
     splits = train_test_split(X,y,test_size=0.1, random_state=0)
     X_train ,X_test, y_train ,y_test = splits
 
-
-    #pickle()
     print('Now training and saving the model.....')
+
+    #Train model and serialize model.
     serialize_model(construct_model)
     #pickle_model(construct_model)
-    #model = online_model()
-    #model = construct_model()
+    
     
--- app.py
+++ app.py
@@ -4,44 +4,22 @@ import gzip
 from os.path import join,abspath,curdir
 from pathlib import Path
 import dill
-import pickle
-from html import unescape
-import spacy
-from spacy.lang.en import STOP_WORDS
-nlp = spacy.load('en_core_web_sm',disable = ['ner','parser','tagger'])
 
 app = Flask(__name__)
 
+'''This webapp basically works by loading trained model from dill file when it receives a post request
+    and predicting the sentiment of the text passed with the post request.
+    App also contains links to my completed machine learning projects.'''
 
-days = ['monday','tuesday','wednesday', 'thursday', 'friday', 'saturday','sunday','sun','mon','tue','wed','thur','fri','sat']
-
-def remove_day(doc):
-    doc = doc.lower()
-    for word in doc.split(' '):
-        if word in days:
-            doc = doc.replace(word,'')
-    return doc
-
-
-def preprocessor(doc):
-    doc = remove_day(doc)
-    return unescape(doc)
-
-def tokenizer(doc):
-    return [word.lemma_ for word in nlp(doc)]
-
-
-
-
+#Create platform specific absolute path to serialized model
 cwd = abspath(curdir)
-model_path = join(cwd, 'sentiment_ng_model.pkl')
-path = Path(model_path).as_posix()
+model_path = join(cwd, 'sentiment_ng_model.dill.gz')
+path = Path(model_path)
 
 
 @app.route('/about')
 def about():
-    #return render_template('developer.html')
-    return path
+    return render_template('developer.html')
 
 @app.route('/projects')
 def projects():
@@ -52,20 +30,23 @@ def projects():
 def main():
     return redirect('/index')
 
-
 @app.route('/index', methods=['GET'])
 def index():
     return render_template('index.html')
 
-
+#This is the function that handles prediction of tweets passed in the post request.
 @app.route('/predict', methods=['GET','POST'])
 def predict():
     if request.method == 'GET':
         tweet = request.args.get('tweet')
     else:
         tweet = request.form['text']
-    with open(path, 'rb') as f:
-        model = pickle.load(f)
+    
+    #Load trained model from file
+    with gzip.open(path, 'rb') as f:
+        model = dill.load(f)
+
+    #predict and round up the probability result of the prediction.
     proba = round(model.predict_proba([tweet])[0,1]* 100,2)
     if (proba <= 52 and proba >= 48):
         sent = -1
@@ -73,7 +54,8 @@ def predict():
         sent = 1
     else: 
         sent = 0
-    
+
+    #return results
     return render_template('predict.html', sent = sent , proba= proba)
 
 
